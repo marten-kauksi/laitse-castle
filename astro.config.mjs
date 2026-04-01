0898e69a5fcb6e1015c50a5e68e10a9f13b13c66
Marten Kauksi <support@rankup.so>
2026-04-01 22:23:43 +0200

    small updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://laitseloss.ee',
+  site: 'https://laitsecastle.ee',
   i18n: {
     defaultLocale: 'et',
     locales: ['et', 'en'],
